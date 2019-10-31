56925f9073457c57b203eccb21a27e44d377b844
FawnYan <47753777+FawnYan@users.noreply.github.com>
2019-10-31 17:34:52 -0400

    Create 3618 Halloween.R

---
+++ 3618 Halloween.R
@@ -0,0 +1,9 @@
+r3 <- c("1","2","3","4","5","6")
+r4 <- c("1","2","3","4","5","6")
+x = c(r3,r4)
+m2 <- matrix(x, nrow=2, ncol=6)
+rownames(m2) = c("witch", "werewolf")
+colnames(m2) = c("kitkat", "dumdums", "heathbar", "butterfinger", "airheads", "candycorn")
+
+
+
